ea009c1a5d8757783dfe6976e81d5dd344a6c4c5
FateZeros <490293266@qq.com>
2023-06-08 09:52:01 +0800

    add rem

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+// 公共样式
+import '@/assets/styles/common/index.scss';
--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import App from '@/views/app/index.vue';
 import type { Router } from 'vue-router';
 import { setupRouter, router } from '@/router';
 
+import '@/assets';
+
 const app = createApp(App);
 
 export const setupApp = async (router: Router) => {
     try {
         // setupRouter 必须放在最后，因为路由守卫需要使用某些全局方法
         await setupRouter(app, router);
-        
-        app.mount("#app");
+
+        app.mount('#app');
     } catch (error) {
         console.error(error);
     }
-}
+};
 
 setupApp(router);
